b82bac072761718362ab8be02c879cd73bf6b124
zyhon96 <52708436+zyhon96@users.noreply.github.com>
2021-05-17 10:53:33 +0100

    Update adedoyinOyebanji.js

--- adedoyinOyebanji.js
+++ adedoyinOyebanji.js
@@ -4,15 +4,13 @@
 // 1ST TASK
 const convertFahrToCelsius = (n) => {
     if (Array.isArray(n)) {
-        console.log(` ${JSON.stringify(n)}is not a valid number but Array`)
-        return ` ${JSON.stringify(n)} is not a valid number but Array`
+        return ` ${JSON.stringify(n)} is not a valid number but Array`;
     }
 
     const inputToNumber = +n
     if (typeof inputToNumber !== "number" || isNaN(n)) {
 
-        console.log(`${JSON.stringify(n)} not a valid number but ${typeof (n)}`);
-        return `${JSON.stringify(n)} is not a valid number but ${typeof (n)}`
+        return `${JSON.stringify(n)} is not a valid number but ${typeof (n)}`;
     }
 
     const fahrenheit = (inputToNumber - 32) * 5 / 9
@@ -30,8 +28,7 @@ console.log(convertFahrToCelsius());
 const checkYuGiOh = (n) => {
     const number = +n;
     if (typeof number !== "number" || isNaN(number)) {
-        console.log(`invalid parameter: ${n}`)
-        return `invalid parameter: ${n}`
+        return `invalid parameter: ${n}`;
     }
     const arr = Array.from(Array(number).keys())
     const newarr = arr.map(i => i + 1);
